3bf6b71e1280f5e9f698a27e4357505ceaf770bc
Alexey <kirinalex.dev@yandex.ru>
2023-02-24 20:33:37 +0700

    +

--- src/main/java/Dijkstras_baeldung.java
+++ src/main/java/Dijkstras_baeldung.java
@@ -1,4 +1,4 @@
-import lombok.Data;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -46,16 +46,16 @@ public class Dijkstras_baeldung {
 class Dijkstra {
     public static Graph6 calculateShortestPathFromSource(Graph6 graph6, Node source) {
         source.setDistance(0);
-        Set<Node> settledNodes = new HashSet<>();
-        Set<Node> unsettledNodes = new HashSet<>();
+        var settledNodes = new HashSet<Node>();
+        var unsettledNodes = new HashSet<Node>();
         unsettledNodes.add(source);
 
-        while (unsettledNodes.size() != 0) {
-            Node currentNode = getLowestDistanceNode(unsettledNodes);
+        while (!unsettledNodes.isEmpty()) {
+            var currentNode = getLowestDistanceNode(unsettledNodes);
             unsettledNodes.remove(currentNode);
             for (Map.Entry<Node, Integer> adjacencyPair : currentNode.getAdjacentNodes().entrySet()) {
-                Node adjacentNode = adjacencyPair.getKey();
-                Integer edgeWeigh = adjacencyPair.getValue();
+                var adjacentNode = adjacencyPair.getKey();
+                var edgeWeigh = adjacencyPair.getValue();
 
                 if (!settledNodes.contains(adjacentNode)) {
                     CalculateMinimumDistance(adjacentNode, edgeWeigh, currentNode);
@@ -67,11 +67,12 @@ class Dijkstra {
         return graph6;
     }
 
+    //!!! переделать на приоритетную очередь
     private static void CalculateMinimumDistance(Node evaluationNode, Integer edgeWeigh, Node sourceNode) {
-        Integer sourceDistance = sourceNode.getDistance();
+        var sourceDistance = sourceNode.getDistance();
         if (sourceDistance + edgeWeigh < evaluationNode.getDistance()) {
             evaluationNode.setDistance(sourceDistance + edgeWeigh);
-            LinkedList<Node> shortestPath = new LinkedList<>(sourceNode.getShortestPath());
+            var shortestPath = new LinkedList<>(sourceNode.getShortestPath());
             shortestPath.add(sourceNode);
             evaluationNode.setShortestPath(shortestPath);
         }
@@ -80,6 +81,7 @@ class Dijkstra {
     private static Node getLowestDistanceNode(Set<Node> unsettledNodes) {
         Node lowestDistanceNode = null;
         int lowestDistance = Integer.MAX_VALUE;
+
         for (Node node : unsettledNodes) {
             int Distance = node.getDistance();
             if (Distance < lowestDistance) {
