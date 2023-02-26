f09886ba111859c2e2ab3f022b83d1bb36ba7a54
Alexey <kirinalex.dev@yandex.ru>
2023-02-26 09:01:35 +0700

    +

--- src/main/java/leetcode/LongestCommonPrefix.java
+++ src/main/java/leetcode/LongestCommonPrefix.java
@@ -18,40 +18,26 @@ public class LongestCommonPrefix {
         //
         // Сложность:  ДлинаКороткого * КолвоСлов
 
+        if (strs.length == 0) {
+            return "";
+        }
+
         var pref = new StringBuilder();
         boolean end = false;
         String a;
         String b;
 
-        //!!! for переделать на while
-        //!!! учесть что массив может быть из одной строки
         for (int i = 0; i < strs[0].length(); i++) {
             a = strs[0].substring(i, i + 1);
             for (int j = 1; j < strs.length; j++) {
                 b = strs[j].substring(i, i + 1);
-                if (b == "" || !b.equals(a)) {
-                    end = true;
-                    break;
+                if (!b.equals(a)) {
+                    return pref.toString();
                 }
             }
-            if (end) break;
             pref.append(a);
         }
 
         return pref.toString();
     };
-
-    public static void main(String[] args) {
-//        Input: strs = ["flower","flow","flight"]
-//        Output: "fl"
-        var res1 = longestCommonPrefix(new String[]{"flower","flow","flight"});
-        System.out.println(res1);
-
-        var res2 = longestCommonPrefix(new String[]{"dog","racecar","car"});
-        System.out.println(res2);
-//
-//        Input: strs = []
-//        Output: ""
-//        Explanation: There is no common prefix among the input strings.
-    }
 }
---
+++ src/test/java/leetcode/LongestCommonPrefixTest.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+import static leetcode.LongestCommonPrefix.longestCommonPrefix;
+
+class LongestCommonPrefixTest {
+
+    @Test
+    void longestCommonPrefixTest() {
+        assert "fl".equals(longestCommonPrefix(new String[]{"flower","flow","flight"}));
+        assert "".equals(longestCommonPrefix(new String[]{"dog","racecar","car"}));
+        assert "".equals(longestCommonPrefix(new String[]{}));
+        assert "abc".equals(longestCommonPrefix(new String[]{"abc"}));
+    }
+}
